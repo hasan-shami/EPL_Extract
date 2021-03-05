5e5e87862cd6fd6fa2cdf5bfc74dbf78376093b4
hsnsh <hsn.shami@gmail.com>
2021-03-05 16:36:00 +0200

    first commit

--- main.py
+++ main.py
@@ -5,6 +5,7 @@
 import pandas as pd
 import numpy as np
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
 
 ## Information on Computer, League, Team, Years, Competition
@@ -14,8 +15,11 @@ team= "Chelsea"    # Note, may want to scrape for exact name here later
 Year= "2019-2020"
 Competition="" # Dropdown menu with multiple selections
 
+
+
 # Edit with your chrome driver executable location
-driver = webdriver.Chrome('/Users/{0}/Documents/chromedriver-win32/chromedriver'.format(ComputerUsername))
+path_chromedriver = r'C:\Users\{0}\Documents\chromedriver-win32\chromedriver.exe'.format(ComputerUsername)
+driver = webdriver.Chrome(executable_path=path_chromedriver)
 
 driver.get('https://fbref.com/en/squads/cff3d9bb/{0}/{1}-Stats'.format(Year, team))
 
